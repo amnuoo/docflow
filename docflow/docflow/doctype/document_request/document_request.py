dc1102b392d792c09769b1e860d7c7a8d5ed7450
amnuoo <ameenahsan.blvk@gmail.com>
2025-11-21 06:32:08 +0000

    looping through child table rows

--- docflow/docflow/doctype/document_request/document_request.py
+++ docflow/docflow/doctype/document_request/document_request.py
@@ -8,3 +8,7 @@ class DocumentRequest(Document):
     def before_insert(self):
         if not self.requested_by:
             self.requested_by = frappe.session.user
+
+    def validate(self):
+
+        new_rows = [v for v in self.document_version if not v.version_no]        
